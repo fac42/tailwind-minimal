6acf8bcb8be1a54f34b0139b416557a4c7dc1528
Alex Chang <alexchang42@outlook.com>
2021-07-06 16:30:25 -0400

    Install tailwind and setup minimal example

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<main class="h-screen w-full bg-[coral]">
+		<div class="container mx-auto h-full flex justify-center items-center">
+			<HelloWorld msg="Hello World"/>
+		</div>
+	</main>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: "App",
+	components: {
+		HelloWorld
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="postcss" scoped>
 </style>
